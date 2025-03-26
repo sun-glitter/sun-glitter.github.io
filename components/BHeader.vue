fc7edf9f5f0100b0d728606bb758de60c6df79c1
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2025-03-26 23:44:31 +0900

    Update LOGO

--- components/BHeader.vue
+++ components/BHeader.vue
@@ -4,7 +4,7 @@
       class="sticky top-0 z-10 w-full border-b-4 flex justify-center items-center bg-slate-100 hover:bg-slate-200"
     >
       <div class="text-center py-4 text-2xl font-bold">
-        <img src="~/assets/images/icon.svg" alt="logo" class="w-20 h-20" />
+        <img src="~/assets/images/icon.png" alt="logo" class="w-20 h-20" />
       </div>
     </NuxtLink>
 </template>
